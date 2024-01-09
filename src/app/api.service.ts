cb8c399d40586e1bb7c6b156f72c74c4602a6f3b
Ferida Papracanin <ferida.papracanin@heig-vd.ch>
2024-01-09 09:39:32 +0100

    erreur dotenv

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private apiUrl = 'https://thenicheapp.onrender.com/';  // Remplacez cela par l'URL réelle de votre API
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Exemple de méthode pour effectuer un appel GET à l'API
+  getSomeData(): Observable<any> {
+    const url = `${this.apiUrl}/endpoint`;
+    return this.http.get(url);
+  }
+
+  // Ajoutez d'autres méthodes pour les appels POST, PUT, DELETE, etc.
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,12 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+const envPath = path.resolve(__dirname, '../.env'); // Chemin absolu vers le fichier .env
+
+const result = dotenv.config({ path: envPath });
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: result.parsed?.API_KEY || 'mongodb+srv://admin:TheNiche1234@clustertheniche.dzl3a3c.mongodb.net/?retryWrites=true&w=majority',
+  dbPassword: result.parsed?.DB_PASSWORD || 'TheNiche1234',
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
